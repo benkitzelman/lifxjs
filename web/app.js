70271c83170451a4a4651ff415d2d1b6519025a3
Ben Kitzelman <benkitzelman@gmail.com>
2014-01-08 17:22:10 +1100

    preventing invalid int errors

--- web/app.js
+++ web/app.js
@@ -7,10 +7,10 @@ app.use('/', express.static(__dirname + '/static'));
 app.get('/set/bulb1', function(req, res, next) {
 	var bulb1 = req.query.bulb1;
 	// scale it
-	var hue = bulb1.h * (65536 / 360)
-	var sat = bulb1.s * (65536 / 0.8)
-	var lum = bulb1.l * (65536 / 1)
-	//console.log(hue, sat, lum);
+	var hue = parseInt(bulb1.h * (65536 / 360));
+	var sat = parseInt(bulb1.s * (65536 / 0.8));
+	var lum = parseInt(bulb1.l * (65536 / 1));
+
 	lx.lightsColour(hue, sat, lum, 0x0dac, 0);
 	res.end("OK");
 });
